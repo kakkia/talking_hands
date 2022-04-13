993b5d2111d4cd6ae4c7b7f76e646bf60e95a82a
kakkia <98701415+kakkia@users.noreply.github.com>
2022-04-13 17:23:52 +0200

    Update streamlit_hp.py

--- streamlit_hp.py
+++ streamlit_hp.py
@@ -16,13 +16,13 @@ def main():
 When speaking Italian it is mandatory that you express your emotions with your hands. \n
 #TalkingHands is the first image classification app that will help you to express your emotions when you just can’t find the words. \n
 For my final project in Data Science at the WBS Coding School, I trained a convolutional neural network to automatically classify three different hand gestures commonly used in Italy. You will be finally able to understand Italians by looking at their hands!\n
-Do you want to give us a hand? You can upload a picture of your gesture and you will find how what it means and how to use it.\n
 So far the model has only been trained with:""")
     
     from PIL import Image
     hp = Image.open('images/gestures.jpg')
     st.image(hp, width=None)
     
+    st.write("Do you want to give us a hand? You can upload a picture of your gesture and you will find how what it means and how to use it.")
     #load_image()
 
 if __name__ == '__main__':
